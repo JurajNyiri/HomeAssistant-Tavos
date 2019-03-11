a3d5cfc4f3f575062732fecf9e52d736270b195e
Juraj Nyiri <jnyiri@sk.ibm.com>
2019-03-11 15:48:23 +0100

    Add: initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# tavosPy
+Processes water outages from Tavos and provides them inside Home Assistant
+
+## Installation:
+Copy file sensor.py to custom_components/tavosWaterOutage/sensor.py
+
+## Usage:
+Add to configuration.yaml:
+
+```
+sensor:
+  - platform: tavosWaterOutage
+    name: [Optional - name]
+    monitored_conditions: [Optional, list of cities/streets to monitor]
+        - Example1
+        - Example2
+```
+
+- State of the entity becomes nonempty if there is upcoming water outage for one of your monitored cities or streets.
+- Attributes of the entity list all the outages.
+
+# Disclaimer
+
+Project and/or author is in no way associated with Tavos and provides this code completely free, according to LICENSE file.
---
+++ sensor.py
@@ -0,0 +1,92 @@
+"""
+Support for Tavos Water Outage
+configuration.yaml
+sensor:
+  - platform: tavosWaterOutage
+"""
+REQUIREMENTS = ['tavosPy==0.1.2']
+
+import logging
+import json
+import voluptuous as vol
+import re
+
+from datetime import timedelta,datetime
+
+from homeassistant.components.sensor import (PLATFORM_SCHEMA)
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import (CONF_NAME, CONF_MONITORED_CONDITIONS)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.util import Throttle
+
+DEFAULT_NAME = "TAVOS Water Outage Sensor"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=[]): vol.All(cv.ensure_list)
+})
+
+_LOGGER = logging.getLogger(__name__)
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=600) #10 minutes
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    import tavosPy
+    add_devices([TavosWaterOutage(config)])
+
+
+class TavosWaterOutage(Entity):
+
+    def __init__(self, config):
+        import tavosPy
+        self._state = ""
+        self._all_outages = {}
+        self._name = config.get(CONF_NAME)
+        self._monitored_conditions = config.get(CONF_MONITORED_CONDITIONS)
+        self.tavospy = tavosPy.TavosPy()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @Throttle(MIN_TIME_BETWEEN_SCANS)
+    def update(self):
+        self.manualUpdate()
+
+    @property
+    def device_state_attributes(self):
+        return self._all_outages
+
+    def manualUpdate(self):
+        self.tavospy.update()
+        self._state = ""
+
+        tavosData = self.tavospy.getData()
+    
+        for waterOutage in tavosData:
+            for city in self._monitored_conditions:
+                attribute = ""
+                if(waterOutage['date']['start']):
+                    attribute = attribute + waterOutage['date']['start'].strftime("%d.%m.%Y %H:%M")
+                if(waterOutage['date']['end']):
+                    attribute = attribute + " - " + waterOutage['date']['end'].strftime("%d.%m.%Y %H:%M")
+                
+                value = ""
+                if(waterOutage['city'] != ""):
+                    value = value + waterOutage['city']
+                if(waterOutage['street'] != ""):
+                    value = value + " (" + waterOutage['street'] + ")"
+                if(waterOutage['typeOfDefect'] != ""):
+                    value = value + " - " + waterOutage['typeOfDefect']
+                if(waterOutage['notes'] != ""):
+                    value = value + " (" + waterOutage['notes'] + ")"
+                self._all_outages[attribute] = value
+                
+                if((city in waterOutage['city'] or city in waterOutage['street']) and (datetime.now() < waterOutage['date']['start'] or datetime.now() < waterOutage['date']['end'])):
+                    if(self._state != ""):
+                        self._state = self._state + "\r\n" + attribute + ": " + value
+                    else:
+                        self._state = attribute + ": " + value
